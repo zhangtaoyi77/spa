4e4375e0c68d780b9569301ed93e9736ea743240
wangding <408542507@qq.com>
2019-04-16 15:53:08 +0800

    All Files Commit

---
+++ 10-timer-button/index.js
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>hello</title>
+  <link rel="stylesheet" href="layout.css">
+  <link rel="icon" href="https://i.wangding.in/favicon.ico" size="48*48" type="image/png">
+</head>
+<body>
+  <input type="button" value="同意" disabled>
+  <script src="//apps.bding.com/libs/jquery/2.1.4/jquery.min.js"></script>
+</body>
+</html>
