b6ed03cd5ee04db550c71304183eceb6a5c8b2c8
wangding <408542507@qq.com>
2019-04-16 16:26:53 +0800

    range

---
+++ 12-range/main.js
@@ -0,0 +1,9 @@
+$(function(){
+        var $range = $('input[type="range"]');
+        var $txtAge=$('#age');
+        $range.change(function(){
+              $txtAge.html($range.val());
+            });
+    });
+      
+ 
